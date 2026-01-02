0440ee6760dc3d9e769c0ba80f5b413689eb71ab
csumie <csurdanivia@gmail.com>
2026-01-01 22:12:57 -0500

    JavaScript update

--- js/main.js
+++ js/main.js
@@ -1,5 +1,11 @@
 window.addEventListener('keydown', function(e) {
   // console.log(e.keyCode);
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-  console.log(audio);
+  // console.log(audio);
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+  console.log(key);
+  if(!audio)return; //will stop function from running
+  audio.currentTime=0; //will jump to the start
+  audio.play();
+  key.classList.add('playing');
 });
